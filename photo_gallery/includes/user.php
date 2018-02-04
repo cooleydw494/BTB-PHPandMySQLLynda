042c580eebde2905c01d9ebd560eba10e2240f39
cooleydw494 <cooleydw494@gmail.com>
2018-02-04 16:32:21 -0500

    expanded on user class, added ability to find all or one user and return as an object with database value attributes. added session class, user auth and admin/login page

--- photo_gallery/includes/user.php
+++ photo_gallery/includes/user.php
@@ -1,8 +1,14 @@
 <?php
 //we're going to need the database
-require_once '../includes/database.php';
+require_once 'database.php';
 
 class User {
+
+  public $id;
+  public $username;
+  public $password;
+  public $first_name;
+  public $last_name;
 
   public static function find_all() {
     $result_set = self::find_by_sql('SELECT * FROM users');
@@ -11,14 +17,68 @@ class User {
 
   public static function find_by_id($id=0) {
     global $database;
-    $result_set = self::find_by_sql("SELECT * FROM users WHERE id = {$id}");
-    $found = $database->fetch_array($result_set);
-    return $found;
+    $result_array = self::find_by_sql("SELECT * FROM users WHERE id = {$id}");
+    return !empty($result_array) ? array_shift($result_array) : false;
   }
 
   public static function find_by_sql($sql = '') {
     global $database;
     $result_set = $database->query($sql);
-    return $result_set;
+    $object_array = [];
+    while ($row = $database->fetch_array($result_set)) {
+      $object_array[] = self::instantiate($row);
+    }
+    return $object_array;
+  }
+
+  public static function authenticate($username = '', $password = '') {
+    global $database;
+    $username = $database->escape_value($username);
+    $password = $database->escape_value($password);
+
+    $sql = "SELECT * FROM users ";
+    $sql .= "WHERE username = '{$username}' ";
+    $sql .= "AND password = '{$password}' ";
+    $sql .= "LIMIT 1";
+
+    $result_array = self::find_by_sql($sql);
+    return !empty($result_array) ? array_shift($result_array) : false;
+  }
+
+  public function full_name() {
+    if (isset($this->first_name) && isset($this->last_name)) {
+      return $this->first_name . ' ' . $this->last_name;
+    } else {
+      return '';
+    }
+  }
+
+  private static function instantiate($record) {
+    $object = new self;
+
+    // long form approach (not great if you have many attributes)
+    // $object->id = $record['id'];
+    // $object->username = $record['username'];
+    // $object->password = $record['password'];
+    // $object->first_name = $record['first_name'];
+    // $object->last_name = $record['last_name'];
+
+    //more dynamic short-form approach
+    foreach($record as $attribute => $value) {
+      if ($object->has_attribute($attribute)) {
+        $object->$attribute = $value;
+      }
+    }
+
+    return $object;
+  }
+
+  private function has_attribute($attribute) {
+    // get_object_vars returns an associative array with all attributes (including private ones)
+    // as the keys and their current values as the value
+    $object_vars = get_object_vars($this);
+    // array_key_exists will return true if the given value is a key in the array
+    // or false if it is not
+    return array_key_exists($attribute, $object_vars);
   }
 }
--- photo_gallery/public/index.php
+++ photo_gallery/public/index.php
@@ -1,43 +1,19 @@
 <?php
 require_once '../includes/database.php';
+require_once '../includes/functions.php';
 require_once '../includes/user.php';
 
-if (isset($database)) {
-  echo 'database set<br />';
-} else {
-  echo 'nope no database set<br/>';
-}
+$user = User::find_by_id(1);
 
-echo $database->escape_value("It's working?");
-
-// $sql = "INSERT INTO users (id, username, password, first_name, last_name) ";
-// $sql .= "VALUES (1, 'cooleydw494', 'secret', 'David', 'Cooley')";
-// $result = $database->query($sql);
-// echo "<br />";
-// var_dump($result);
-
-// $sql = "INSERT INTO users (username, password, first_name, last_name) ";
-// $sql .= "VALUES ('kskoglund', 'secret', 'Kevin', 'Skoglund')";
-// $result = $database->query($sql);
-
-$sql = "SELECT * FROM users ";
-$result = $database->query($sql);
-
-while ($found_user = $database->fetch_array($result)) {
-  echo '<pre>';
-  var_dump($found_user);
-  echo '</pre>';
-}
+echo $user->id . '<br />';
+echo $user->username . '<br />';
+echo $user->password . '<br />';
+echo $user->full_name() . '<br />';
 
 echo '<hr />';
 
-$found_user = User::find_by_id(1);
-echo $found_user['username'];
-
-echo '<hr />';
-
-$all_users = User::find_all();
-while($user = $database->fetch_array($all_users)) {
-  echo 'User: ' . $user['username'] . '<br />';
-  echo 'Name: ' . $user['first_name'] . ' ' . $user['last_name'] . '<br />';
+$users = User::find_all();
+foreach($users as $user) {
+  echo 'User: ' . $user->username . '<br />';
+  echo 'Name: ' . $user->full_name() . '<br />';
 }
