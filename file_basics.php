05ef1adb708728f726a11d64cff3dda698e79036
cooleydw494 <cooleydw494@gmail.com>
2018-02-05 21:41:37 -0500

    basic linux based file permissions stuff

---
+++ file_basics.php
@@ -0,0 +1,30 @@
+<?php
+
+echo __FILE__ . '<br />';
+echo __LINE__ . '<br />';
+echo dirname(__FILE__) . '<br />';
+echo __DIR__ . '<br />';
+
+echo file_exists(__FILE__) ? 'yes' : 'no';
+echo '<br />';
+
+echo file_exists(__DIR__ . '/new.php') ? 'yes' : 'no';
+echo '<br />';
+
+echo file_exists(__DIR__) ? 'yes' : 'no';
+echo '<br />';
+
+echo is_file(__DIR__ . '/new.php') ? 'yes' : 'no';
+echo '<br />';
+
+echo is_file(__DIR__) ? 'yes' : 'no';
+echo '<br />';
+
+echo is_dir(__DIR__ . '/new.php') ? 'yes' : 'no';
+echo '<br />';
+
+echo is_dir(__DIR__) ? 'yes' : 'no';
+echo '<br />';
+
+echo is_dir('..') ? 'yes' : 'no';
+echo '<br />';
