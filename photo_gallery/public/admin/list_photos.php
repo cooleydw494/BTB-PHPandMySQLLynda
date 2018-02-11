792461bcf9c7db0e0d28ad81065a719a4a0542c3
cooleydw494 <cooleydw494@gmail.com>
2018-02-11 15:15:29 -0500

    pagination all the way through

--- photo_gallery/public/admin/list_photos.php
+++ photo_gallery/public/admin/list_photos.php
@@ -1,8 +1,21 @@
 <?php require_once("../../includes/initialize.php"); ?>
 <?php if (!$session->is_logged_in()) { redirect_to("login.php"); } ?>
 <?php
-  // Find all the photos
-  $photos = Photograph::find_all();
+
+  $page = $_GET['page'] ?? 1;
+  $per_page = 5;
+  $total_count = Photograph::count_all();
+
+  // // Find all the photos
+  // $photos = Photograph::find_all();
+  // USE PAGINATION
+  $pagination = new Pagination((int)$page, $per_page, $total_count);
+
+  $sql = "SELECT * FROM photographs ";
+  $sql .= "LIMIT {$per_page} ";
+  $sql .= "OFFSET {$pagination->offset()}";
+  $photos = Photograph::find_by_sql($sql);
+
 ?>
 <?php include_layout_template('admin_header.php'); ?>
 
@@ -42,5 +55,29 @@
 </table>
 <br />
 <a href="photo_upload.php">Upload a new photograph</a>
+
+<div id="pagination" style="clear:both;">
+	<?php
+	if ($pagination->total_pages() > 1) {
+
+		if ($pagination->has_previous_page()) {
+			echo "<a style=\"margin-left:2em;\" href=\"list_photos.php?page=" . $pagination->previous_page() . "\">&laquo;Previous</a>";
+		}
+
+		for ($i = 1; $i <= $pagination->total_pages(); $i++) {
+			if ($i == $pagination->current_page) {
+				echo "<a class=\"selected\" style=\"margin-left:1em;\" href=\"list_photos.php?page=" . $i . "\">{$i}</a>";
+			} else {
+				echo "<a style=\"margin-left:1em;\" href=\"list_photos.php?page=" . $i . "\">{$i}</a>";
+			}
+		}
+
+		if ($pagination->has_next_page()) {
+			echo "<a style=\"margin-left:2em;\" href=\"list_photos.php?page=" . $pagination->next_page() . "\">Next &raquo;</a>";
+		}
+
+	}
+	 ?>
+</div>
 
 <?php include_layout_template('admin_footer.php'); ?>
